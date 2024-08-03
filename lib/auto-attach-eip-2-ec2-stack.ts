ce07068e5d67e05014e231c1bd94c586d94ae54b
dahuahua <38651850@qq.com>
2024-08-03 22:05:26 +0800

    Initial commit

---
+++ lib/auto-attach-eip-2-ec2-stack.ts
@@ -0,0 +1,16 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class AutoAttachEip2Ec2Stack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'AutoAttachEip2Ec2Queue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
